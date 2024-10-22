12997eec9c399d1a3a9065582741f4e4f20267b8
KarenS22 <karenquito2004@gmail.com>
2024-10-22 10:53:49 -0500

    compomets

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { ContenidoComponent } from './pages/contenido/contenido.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: 'pages\contenido', component: ContenidoComponent}
+];
